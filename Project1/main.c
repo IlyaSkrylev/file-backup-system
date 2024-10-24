6b52a9c2d40afb30ca4e40c8ac596edd2ddad9af
skrylev <ilya.skr2004@gmail.com>
2024-10-25 00:49:39 +0300

    Test commit

--- Project1/main.c
+++ Project1/main.c
@@ -2,5 +2,7 @@
 #include "mainWindow.h"
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    int i = 0;
     return RunWindow(hInstance, nCmdShow);
 }
+// hello world
